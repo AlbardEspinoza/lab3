7f7162e0b6ce5174d917e4f962f02a433770fcf5
albard.espinoza <albard.espinoza@servicenow.com>
2023-04-21 00:31:07 -0700

    Fixed bugs in averageWithoutLowest

--- ArrayTests.java
+++ ArrayTests.java
@@ -28,5 +28,34 @@ public class ArrayTests {
                 int[] input1 = {1, 2, 3};
 		assertArrayEquals(new int[]{ 3, 2, 1 }, ArrayExamples.reversed(input1));
         }
+	
+	@Test
+	public void testAverageWithoutLowest_EmptyArray() {
+		double[] input1 = {};
+		assertEquals(0, ArrayExamples.averageWithoutLowest(input1), 0);
+	}
 
+	@Test
+        public void testAverageWithoutLowest_OneElement() {
+                double[] input1 = {1};
+                assertEquals(0, ArrayExamples.averageWithoutLowest(input1), 0);
+        }	
+	
+	@Test
+        public void testAverageWithoutLowest_EvenAmountOfElements() {
+                double[] input1 = {7, 4, 6, 2};
+                assertEquals(5.666666666666667, ArrayExamples.averageWithoutLowest(input1), 0);
+        }
+
+	@Test
+	public void testAverageWithoutLowest_OddAmountOfElements() {
+                double[] input1 = {9, 3, 3, 7, 1};
+                assertEquals(5.5, ArrayExamples.averageWithoutLowest(input1), 0);
+        }
+	
+	@Test
+	public void testAverageWithoutLowest_WithNegativeElements() {
+                double[] input1 = {1, 4, 3, -20, -20};
+                assertEquals(-3, ArrayExamples.averageWithoutLowest(input1), 0);
+        }
 }
